97c5beb7ddfc3ace9be2d4552677f1a6fe93b7ca
Danila <62520454+VNEShi@users.noreply.github.com>
2023-01-14 19:21:26 +0300

    rename

--- 1_mod3_and_mod5.py
+++ 1_mod3_and_mod5.py
@@ -4,11 +4,6 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5,
 we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
 Find the sum of all the multiples of 3 or 5 below 1000.
-
-Sample Input 2:
-100
-Sample Output 2:
-2318
 """
 n = int(input())
 a = 0
--- 2_fib_mod.py
+++ 2_fib_mod.py
@@ -4,11 +4,6 @@ By starting with 1 and 2, the first 10 terms will be:
 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
 By considering the terms in the Fibonacci sequence whose values do
 not exceed four million,find the sum of the even-valued terms.
-
-Sample Input 1:
-100
-Sample Output 1:
-44
 """
 n = int(input())
 F_0 = 0
